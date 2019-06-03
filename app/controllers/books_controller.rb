da3d092825e89ff3d721ec55d4f7e4d0a047b09e
tesinhong <tesin.0921@gmail.com>
2019-06-03 05:05:28 +0000

    bookers1 完成！

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+
+  def show
+    	@book = Book.find(params[:id])
+  end
+
+  def edit
+	@book = Book.find(params[:id])
+  end
+
+  def top
+  end
+
+  def index
+	@book = Book.new
+	@books = Book.all
+  end
+
+  def create
+        book = Book.new(book_params)
+        if book.save
+	  flash[:notice] = "Book was successfuly created!"
+          redirect_to book_path(book.id)
+        end
+  end
+
+  def update
+	book = Book.find(params[:id])
+	if book.update(book_params)
+	  flash[:notice] = "Book was successfully updated!"
+	  redirect_to book_path(book.id)
+	end
+  end
+
+  def destroy
+	book = Book.find(params[:id])
+	if book.destroy
+	  flash[:notice] = "Book was successfully deleted!"
+	  redirect_to books_path
+	end
+  end
+
+    private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
